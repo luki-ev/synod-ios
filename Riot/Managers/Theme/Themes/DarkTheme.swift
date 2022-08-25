00b6a60a5ec242de7841dee278bc8edd57d3cc94
Christoph Settgast <csett86@web.de>
2022-08-25 21:36:17 +0200

    branding: synod colors

--- Riot/Managers/Theme/Themes/DarkTheme.swift
+++ Riot/Managers/Theme/Themes/DarkTheme.swift
@@ -45,7 +45,7 @@ class DarkTheme: NSObject, Theme {
     var textTertiaryColor: UIColor = UIColor(rgb: 0x8E99A4)
     var textQuinaryColor: UIColor = UIColor(rgb: 0x394049)
 
-    var tintColor: UIColor = UIColor(rgb: 0x0DBD8B)
+    var tintColor: UIColor = UIColor(rgb: 0x32B4B6)
     var tintBackgroundColor: UIColor = UIColor(rgb: 0x1F6954)
     var tabBarUnselectedItemTintColor: UIColor = UIColor(rgb: 0x8E99A4)
     var unreadRoomIndentColor: UIColor = UIColor(rgb: 0x2E3648)
